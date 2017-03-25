0237cfd0f68c5fd1e73e3ea473c4e7cab6d21d5e
monizvalentine <monizvalentine@yahoo.fr>
2017-03-25 07:05:16 -0400

    test

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,18 @@
 class City < ActiveRecord::Base
+  before_validation :geocode
+  
+  private 
+  def geocode
+    if attribute_present?("name")
+       places = Nominatim.search(self.name).limit(1)
+       place = places.first
+      if place
+       self.lattitude = place.lat
+       self.longitude = place.lon
+      end
+    end
+  end
+  
+
+  
 end
