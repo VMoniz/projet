9466d67c767aadb815b54758fff888dbfa97d3fd
monizvalentine <monizvalentine@yahoo.fr>
2017-04-21 10:15:03 -0400

    chargement continu

--- lol/src/app/app.component.ts
+++ lol/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
