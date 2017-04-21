8409ebffc7fda0d490c0de211063d6a3e7e7b62a
monizvalentine <monizvalentine@yahoo.fr>
2017-04-21 09:58:25 -0400

    test

--- lol/src/app/app.component.ts
+++ lol/src/app/app.component.ts
@@ -1,37 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'NANANANA BATMAN' },
-  { id: 12, name: 'Superman' },
-  { id: 13, name: 'Aquaman' },
-  { id: 14, name: 'Flash' },
-  { id: 15, name: 'Green Lantern' },
-  { id: 16, name: 'Green Arrow' },
-  { id: 17, name: 'Wonder Woman' },
-  { id: 18, name: 'Black Canari' },
-  { id: 19, name: 'J\'onn J\'onzz' },
-  { id: 20, name: 'Hawk Girl' }
-];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
-export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes = HEROES;
+export class AppComponent implements OnInit {
+  
+  title = 'Tour of Students';
+  heroes: Hero[];
   selectedHero: Hero;
-  hero: Hero = 
-  {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  onSelect(hero: Hero): void 
-  {
+  
+  
+  constructor(private heroService: HeroService) { }
+  getHeroes(): void {
+    this.heroService
+        .getHeroes()
+        .then(heroes => this.heroes = heroes );
+  }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  
+
 }
---
+++ lol/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'NANANANA BATMAN' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Aquaman' },
+  { id: 14, name: 'Flash' },
+  { id: 15, name: 'Green Lantern' },
+  { id: 16, name: 'Green Arrow' },
+  { id: 17, name: 'Wonder Woman' },
+  { id: 18, name: 'Black Canari' },
+  { id: 19, name: 'J\'onn J\'onzz' },
+  { id: 20, name: 'Hawk Girl' }
+];
